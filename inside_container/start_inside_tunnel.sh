916e186122ce55d2989de33bea920afad4eb0e32
Markus Emmenegger <markus.emmenegger@poemai.ch>
2023-12-03 00:54:00 +0100

    Add support for getting ssh key using aws cli secrets

--- inside_container/start_inside_tunnel.sh
+++ inside_container/start_inside_tunnel.sh
@@ -4,9 +4,19 @@
 # start_inside_tunnel.sh
 ##
 
+TUNNEL_USER=${TUNNEL_USER:-root}
 # Install the SSH private key (ensure this is done securely)
 mkdir -p /root/.ssh
-cat /etc/ssh-key/id_rsa > /root/.ssh/id_rsa
+
+# if aws secret name is given, use it to get the private key
+if [ -n "$AWS_SECRET_NAME" ]; then
+    echo "Getting private key from AWS secret manager"
+    aws secretsmanager get-secret-value --secret-id $AWS_SECRET_NAME --region $AWS_REGION --query SecretString --output text > /root/.ssh/id_rsa
+else
+    echo "Using private key from mounted volume"
+    cat /etc/ssh-key/id_rsa > /root/.ssh/id_rsa
+fi
+
 chmod 600 /root/.ssh/id_rsa
 
 # Start the SSH tunnel
